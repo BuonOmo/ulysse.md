d3e972ef702e88aa6a76f4a4903804d6454322eb
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2021-08-02 07:13:33 +0200

    WIP: first draft

---
+++ assembly/game_of_life.c
@@ -0,0 +1,326 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#ifdef TERMINAL
+#include <stdio.h>
+#include <unistd.h>
+#endif
+#include <emscripten.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PIXEL 5
+#define GAMES 10
+
+#define color(R, G, B, A) (R | G << 8 | B << 16 | A << 24)
+
+#define ALIVE_COLOR color(0xda, 0x09, 0xff, 0xff)
+#define DEAD_COLOR color(0, 0, 0, 0)
+
+typedef struct {
+    int height;
+    int width;
+    bool **board;
+} game_of_life;
+
+game_of_life game[GAMES];
+int started_games = 0;
+unsigned int *data[GAMES];
+
+void EMSCRIPTEN_KEEPALIVE start(int height, int width, int seed, int index);
+unsigned int* EMSCRIPTEN_KEEPALIVE render(int index);
+
+game_of_life init(int height, int width);
+void clear(game_of_life);
+#ifdef TERMINAL
+void show(game_of_life);
+#endif
+void step(game_of_life);
+
+int neighbors(int h, int w, game_of_life);
+
+void EMSCRIPTEN_KEEPALIVE start(int height, int width, int seed, int index) {
+    srand(seed * index);
+    if (game[index].board) clear(game[index]);
+    game[index] = init(height / PIXEL, width / PIXEL);
+    if (!data[index]) data[index] = malloc(height*width*sizeof(unsigned int));
+}
+
+unsigned int* EMSCRIPTEN_KEEPALIVE render(int index) {
+    step(game[index]);
+    for (int i = 0; i < game[index].height; i++) {
+        for (int j = 0; j < game[index].width; j++) {
+            for (int k = 0; k < PIXEL; k++)
+            {
+                for (int l = 0; l < PIXEL; l++)
+                {
+                    data[index][(i * PIXEL + l) * game[index].width * PIXEL + j * PIXEL + k] = game[index].board[i][j] ? ALIVE_COLOR : DEAD_COLOR;
+                }
+            }
+
+        }
+    }
+	return data[index];
+}
+
+game_of_life init(int height, int width) {
+    game_of_life game;
+    bool **board;
+
+    board = malloc(height*sizeof(bool*));
+    for (int i = 0; i < height; i++) {
+        board[i] = malloc(width*sizeof(bool));
+        for (int j = 0; j < width; j++) {
+            board[i][j] = rand() & 1;
+        }
+    }
+    game = (game_of_life){.height = height, .width = width, .board = board};
+    return game;
+}
+
+void clear(game_of_life game) {
+    for (int i = 0; i < game.height; i++) free(game.board[i]);
+    free(game.board);
+}
+
+void step(game_of_life game) {
+    int neighbors_count[game.height][game.width];
+    for (int i = 0; i < game.height; i++) {
+        for (int j = 0; j < game.width; j++) {
+            neighbors_count[i][j] = neighbors(i, j, game);
+        }
+    }
+    for (int i = 0; i < game.height; i++) {
+        for (int j = 0; j < game.width; j++) {
+            switch(neighbors_count[i][j]) {
+                case 2:
+                    game.board[i][j] = game.board[i][j];
+                    break;
+                case 3:
+                    game.board[i][j] = 1;
+                    break;
+                default: // 0-1 && 4-8
+                    game.board[i][j] = 0;
+                    break;
+            }
+        }
+    }
+}
+
+#ifdef TERMINAL
+void show(game_of_life game) {
+    for (int i = 0; i < game.height; i++) {
+        for (int j = 0; j < game.width; j++) {
+            printf("%i", game.board[i][j]);
+        }
+        printf("\n");
+    }
+}
+#endif
+
+int neighbors(int h, int w, game_of_life game) {
+    int count = 0;
+    if (h > 0) {
+        if (w > 0) count += game.board[h - 1][w - 1];
+        count += game.board[h - 1][w];
+        if (w < game.width - 1) count += game.board[h - 1][w + 1];
+    }
+    if (h < game.height - 1) {
+        if (w > 0) count += game.board[h + 1][w - 1];
+        count += game.board[h + 1][w];
+        if (w < game.width - 1) count += game.board[h + 1][w + 1];
+    }
+    if (w > 0) count += game.board[h][w - 1];
+    if (w < game.width - 1) count += game.board[h][w + 1];
+    return count;
+}
+
+#ifdef TERMINAL
+int main()
+{
+    game = init(30, 60);
+
+	int rounds = 1000;
+	for (int i = 0; i < rounds; i++)
+	{
+		step(game);
+		show(game);
+		usleep(100000);
+		if (i < rounds - 1) printf("%c[%iA", 27, game.height);
+	}
+
+    clear(game);
+
+    return 0;
+}
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+/*
+#include <stdlib.h>
+#include <stdbool.h>
+#ifdef TERMINAL
+#include <stdio.h>
+#include <unistd.h>
+#endif
+#include <emscripten.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PIXEL 4
+
+#define color(R, G, B, A) (R | G << 8 | B << 16 | A << 24)
+
+#define ALIVE_COLOR color(0xda, 0x09, 0xff, 0xff)
+#define DEAD_COLOR color(0, 0, 0, 0)
+
+typedef struct {
+    int height;
+    int width;
+    bool **board;
+} game_of_life;
+
+game_of_life game;
+unsigned int *data;
+
+game_of_life init(int height, int width);
+void clear(game_of_life);
+#ifdef TERMINAL
+void show(game_of_life);
+#endif
+void step(game_of_life);
+
+int neighbors(int h, int w, game_of_life);
+
+void EMSCRIPTEN_KEEPALIVE start(int height, int width, int seed) {
+    srand(seed);
+    game = init(height / PIXEL - height % PIXEL, width / PIXEL - width % PIXEL);
+    data = malloc(height*width*sizeof(unsigned int));
+}
+
+void EMSCRIPTEN_KEEPALIVE stop() {
+    clear(game);
+    free(data);
+}
+
+unsigned int* EMSCRIPTEN_KEEPALIVE render() {
+	step(game);
+
+	for (int i = 0; i < game.height; i++) {
+		int iw = i * game.width;
+        for (int j = 0; j < game.width; j++) {
+            for (int k = 0; k < PIXEL; k++)
+            {
+                for (int l = 0; l < PIXEL; l++)
+                {
+    			data[(i * PIXEL + l) * game.width * PIXEL + j * PIXEL + k] = game.board[i][j] ? ALIVE_COLOR : DEAD_COLOR;
+                }
+            }
+
+		}
+	}
+	return &data[0];
+}
+
+game_of_life init(int height, int width) {
+    game_of_life game;
+    bool **board;
+
+    board = malloc(height*sizeof(bool*));
+    for (int i = 0; i < height; i++) {
+        board[i] = malloc(width*sizeof(bool));
+        for (int j = 0; j < width; j++) {
+            board[i][j] = rand() & 1;
+        }
+    }
+    game = (game_of_life){.height = height, .width = width, .board = board};
+    return game;
+}
+
+void clear(game_of_life game) {
+    for (int i = 0; i < game.height; i++) free(game.board[i]);
+    free(game.board);
+}
+
+void step(game_of_life game) {
+    int neighbors_count[game.height][game.width];
+    for (int i = 0; i < game.height; i++) {
+        for (int j = 0; j < game.width; j++) {
+            neighbors_count[i][j] = neighbors(i, j, game);
+        }
+    }
+    for (int i = 0; i < game.height; i++) {
+        for (int j = 0; j < game.width; j++) {
+            switch(neighbors_count[i][j]) {
+                case 2:
+                    game.board[i][j] = game.board[i][j];
+                    break;
+                case 3:
+                    game.board[i][j] = 1;
+                    break;
+                default: // 0-1 && 4-8
+                    game.board[i][j] = 0;
+                    break;
+            }
+        }
+    }
+}
+
+#ifdef TERMINAL
+void show(game_of_life game) {
+    for (int i = 0; i < game.height; i++) {
+        for (int j = 0; j < game.width; j++) {
+            printf("%i", game.board[i][j]);
+        }
+        printf("\n");
+    }
+}
+#endif
+
+int neighbors(int h, int w, game_of_life game) {
+    int count = 0;
+    if (h > 0) {
+        if (w > 0) count += game.board[h - 1][w - 1];
+        count += game.board[h - 1][w];
+        if (w < game.width - 1) count += game.board[h - 1][w + 1];
+    }
+    if (h < game.height - 1) {
+        if (w > 0) count += game.board[h + 1][w - 1];
+        count += game.board[h + 1][w];
+        if (w < game.width - 1) count += game.board[h + 1][w + 1];
+    }
+    if (w > 0) count += game.board[h][w - 1];
+    if (w < game.width - 1) count += game.board[h][w + 1];
+    return count;
+}
+
+#ifdef TERMINAL
+int main()
+{
+    game = init(30, 60);
+
+	int rounds = 1000;
+	for (int i = 0; i < rounds; i++)
+	{
+		step(game);
+		show(game);
+		usleep(100000);
+		if (i < rounds - 1) printf("%c[%iA", 27, game.height);
+	}
+
+    clear(game);
+
+    return 0;
+}
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+*/
